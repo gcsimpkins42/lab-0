9ad2c74e3a49fa725d98fd6dbc9d4022c965e075
Sawyer B <sawyerblanchford@gmail.com>
2024-09-19 09:55:41 -0500

    here is the file we worked on monday

---
+++ Driver.cpp
@@ -0,0 +1,72 @@
+/*
+    Title: Program One
+	Authors: Sawyer Blanchford, Anthony Cosentino, Zachary Jones, Gabe Simpkins
+	Date: 9/16/2024
+	Purpose: 
+*/
+/*
+forest:
+
+player has choice of 2?3? things
+
+    switch statement to call each encounter/interaction
+
+player has choice of 2?3? things
+
+    switch statement to call each encounter/interaction
+    
+player has choice of 2?3? things
+
+    switch statement to call each encounter/interaction
+
+player has choice of 2?3? things
+
+    switch statement to call each encounter/interaction
+
+player has boss fight
+
+
+
+*/
+
+#include<iostream>
+using namespace std;
+
+class Forest 
+{
+    private:
+
+
+    public:
+
+
+    
+};
+
+class Data
+{
+
+	private:
+    
+};
+
+class Interactions
+{
+
+    private:
+
+    public:
+
+};
+
+
+
+
+
+int main (){
+    
+
+    return 0;
+}
+
+// testiung for git 
